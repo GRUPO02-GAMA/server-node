244f0fb24da97f2b4e01a8d264dc8dbc4efe749e
Janice Caldeira <janicecaldeira@gmail.com>
2022-09-09 20:33:26 -0300

    feat: add update and delete functions

--- js/update.js
+++ js/update.js
@@ -3,9 +3,11 @@ const config = require("../database/config");
 
 const connection = mysql.createConnection(config);
 
-const sql = ``;
+const sql = `UPDATE agenda
+            SET name= ?, email= ?, age= ?, gender=?
+            WHERE id = ?`;
 
-const data = [];
+const data = ["Katharina Cureton", "kcureton0@cnet.com", "56", "F", 1];
 
 connection.query(sql, data, (err, result, fields) => {
   if (err) throw err;
