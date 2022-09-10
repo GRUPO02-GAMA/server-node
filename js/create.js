e48cb3e4ce3133a356e32c51900522c39a8701b9
Jayneanderson <aprenderprogramacao001@gmail.com>
2022-09-10 07:28:22 -0300

    refactor: modify sql statement

--- js/create.js
+++ js/create.js
@@ -5,9 +5,9 @@ const connection = mysql.createConnection(config);
 
 const person = cadastrar();
 
-const sql = `insert into agenda values ('?','?','?','?')`;
+const sql = `insert into agenda (name, email, age, gender) values (?, ?, ?, ?)`;
 
-const data = [`${person.name}`, `${person.email}`, `${person.age}`, `${person.gender}`];
+const data = [person.name, person.email, person.age, person.gender];
 
 connection.query(sql, data, (err, result, fields) => {
   if (err) throw err;
@@ -20,10 +20,15 @@ connection.query(sql, data, (err, result, fields) => {
 connection.end();
 
 function cadastrar() {
-  let name = document.getElementById('nome').value;
+/*   let name = document.getElementById('nome').value;
   let email = document.getElementById('email').value;
   let age = document.getElementById('idade').value;
-  let gender = document.getElementById('genero').value;
+  let gender = document.getElementById('genero').value; */
+
+  let name = 'Jay2';
+  let email = "jay1@email.com";
+  let age = 26;
+  let gender = "M";
   
   const record = {
     name: name,
